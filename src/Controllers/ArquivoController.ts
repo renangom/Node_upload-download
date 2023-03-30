d09d90791e776d9f0e9fbd92b6b2a9b63001b96e
Renan <renan23eduardo@gmail.com>
2023-03-30 07:41:39 -0300

    Create upload logic

---
+++ src/Controllers/ArquivoController.ts
@@ -0,0 +1,74 @@
+import {Db, GridFSBucket, ObjectId} from 'mongodb'
+import path from 'path';
+import fs from 'fs';
+
+
+enum ErroUpLoad {
+    OBJETO_ARQUIVO_INVALIDO = 'Objeto de arquivo inválido',
+    ERRO_GRAVACAO = 'Não foi possível gravar o arquivo no banco de dados'
+}
+
+export class ArquivoController {
+    private bd: Db
+    private caminhoDiretorioArquivos: string;
+
+    constructor(bd: Db) {
+        this.bd = bd;
+
+        this.caminhoDiretorioArquivos = path.join(__dirname, '..', '..', 'arquivos_temporarios');
+        if(!fs.existsSync(this.caminhoDiretorioArquivos)) {
+            fs.mkdirSync(this.caminhoDiretorioArquivos);
+        }
+    }
+
+    private ehObjetoArquivoValido (objArquivo:any): boolean {
+        return objArquivo
+                && 'name' in objArquivo 
+                && 'data' in objArquivo 
+                && objArquivo['name'] 
+                && objArquivo['data']
+    }
+
+    private inicializarBucket(): GridFSBucket {
+        return new GridFSBucket(this.bd, {
+            bucketName: 'Arquivos'
+        })
+    }
+ 
+    realizarUpload(objArquivo:any) : Promise<ObjectId> {
+        return new Promise((resolve, reject) => {
+            if(this.ehObjetoArquivoValido(objArquivo)){
+                const bucket = this.inicializarBucket();
+    
+    
+                const nomeArquivo = objArquivo['name'];
+                const conteudoArquivo = objArquivo['data'];
+                const nomeArquivoTemp = `${nomeArquivo}_${(new Date().getTime())}`
+    
+                const caminhoArquivoTemp = path.join(this.caminhoDiretorioArquivos, nomeArquivoTemp);
+                fs.writeFileSync(caminhoArquivoTemp, conteudoArquivo);
+    
+                const streamGridFS = bucket.openUploadStream(nomeArquivo, {
+                    metadata: {
+                        mime: objArquivo['mimetype']
+                    }
+                })
+    
+                const streamLeitura = fs.createReadStream(caminhoArquivoTemp);
+                streamLeitura
+                    .pipe(streamGridFS)
+                    .on('finish', () => {
+                        fs.unlinkSync(caminhoArquivoTemp);
+                        resolve(new ObjectId(`${streamGridFS.id}`))
+                    })
+                    .on('error', (erro) => {
+                        console.log(erro);
+                        reject(ErroUpLoad.ERRO_GRAVACAO)
+                    })
+            } else {
+                reject(ErroUpLoad.OBJETO_ARQUIVO_INVALIDO);
+            }
+        })
+        
+    }
+}
--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -1,4 +1,26 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import path from 'path';
+import fs from 'fs';
 
 
 export const uploadRouter = express.Router();
+
+
+uploadRouter.post('/', (req, res) => {
+    if(!req.files || Object.keys(req.files).length == 0) {
+        return res.status(400).send('Nenhum arquivo recebido');
+    }
+
+    const nomesArquivos = Object.keys(req.files);
+    const diretorio = path.join(__dirname, '..', '..', 'arquivos');
+    if(!fs.existsSync(diretorio)) {
+        fs.mkdirSync(diretorio);
+    }
+
+
+    nomesArquivos.forEach((arquivo) => {
+        
+    })
+
+    res.send('Arquivos gravados no servidor')
+})
